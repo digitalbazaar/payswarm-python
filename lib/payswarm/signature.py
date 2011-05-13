c530a98ea4742baf8dca4316fd89ad6d66200a52
Manu Sporny <msporny@digitalbazaar.com>
2011-05-13 00:08:50 -0400

    Added import of asset/listing from file, digital signature and upload.

--- lib/payswarm/signature.py
+++ lib/payswarm/signature.py
@@ -1,4 +1,13 @@
 """The signature module is used to perform PaySwarm signatures on data."""
+import Crypto.PublicKey.RSA as RSA
+from Crypto.Util.number import long_to_bytes
+import base64
+import binascii
+import copy
+import hashlib
+import json
+import jsonld
+import time
 
 def sign(config, item):
     """Performs a digital signature on a given item.
@@ -8,6 +17,24 @@ def sign(config, item):
 
     Returns a modified item which contains the digital signature information.
     """
+    rval = copy.deepcopy(item)
+    
+    # Generate the signature creation time
+    created = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 
-    return item
+    # Generate the signature    
+    sha1 = hashlib.sha1(jsonld.normalize(item)).hexdigest()
+    key = RSA.importKey(config.get("application", "private-key"))
+    sig_bytes = long_to_bytes(key.sign(sha1, '')[0])
+    signature = binascii.b2a_base64(sig_bytes).strip()
+
+    rval["sig:signature"] = \
+    {
+        "a": "<sig:JsonldSignature>",
+        "dc:created": created,
+        "dc:creator": "<" + config.get("application", "public-key-url") + ">",
+        "sig:signatureValue": signature,
+    }
+    
+    return rval
 
--- lib/payswarm/storage.py
+++ lib/payswarm/storage.py
@@ -1,9 +1,11 @@
 """The storage module is used to remotely store assets and listings."""
 import copy
+import hashlib
 import json
 import jsonld
-import urllib2
 import signature
+import time
+import urllib2
 
 def register_asset(config, asset):
     """Digitally signs the given asset and stores it on the listings service.
@@ -12,41 +14,84 @@ def register_asset(config, asset):
         signatures from as well as the listings service URL.
     asset - the asset to register in JSON format.
 
+    Returns the digitally signed asset.
     Throws an exception if something nasty happens.
     """
     storage_url = config.get("general", "listings-url") + asset["@"]
 
-    # Fill out the config-based information in the given asset
-    signed_asset = copy.deepcopy(asset)
+    # Fill out the config-based information in the asset copy
+    ac = copy.deepcopy(asset)
+    
+    ac["@"] = "<" + storage_url + ">"
+    # FIXME: Need a better way of setting the asset provider. This method
+    # is bound to the dev.payswarm.com PaySwarm Authority software.
+    ac["ps:assetProvider"] = \
+        "<" + config.get("application", 
+            "preferences-url").replace("/preferences", "") + ">"
+    ac["ps:authority"] = \
+        "<" + config.get("general", "config-url") + ">"
+    ac["ps:contentUrl"] = \
+        "<" + storage_url + ">"
 
     # Digitally sign the asset
+    sa = signature.sign(config, ac)
 
     # Upload the asset
-    print storage_url
     req = urllib2.Request(storage_url,
         headers = { "Content-Type": "application/json" },
-        data = json.dumps(signed_asset, sort_keys = True, indent = 3))
+        data = json.dumps(sa, sort_keys = True, indent = 3))
     urllib2.urlopen(req)
+    
+    return sa
 
-def register_listing(config, asset, listing):
+def register_listing(config, signed_asset, listing):
     """Digitally signs the given listing, storing it on the listings service.
 
     config - the configuration to read the private key used for digital 
         signatures from as well as the listings service URL.
-    asset - the listing to register in JSON format.
+    signed_asset - the digitally signed asset that is a part of the
+        listing.
+    listing - the listing to register in JSON format.
 
     Throws an exception if something nasty happens.
     """
     # Fill out the config-based information in the given listing
     storage_url = config.get("general", "listings-url") + listing["@"]
-    signed_listing = copy.deepcopy(listing)
+    lc = copy.deepcopy(listing)
+    asset = copy.deepcopy(signed_asset)
+    del asset["sig:signature"]
+
+    lc["@"] = "<" + storage_url + ">"    
+    # Set all of the AUTOFILL variables
+    for p in lc["com:payee"]:
+        if("AUTOFILL" in p["@"]):
+            # FIXME: Need a better way of setting the payee. This method
+            # is bound to the dev.payswarm.com PaySwarm Authority software.
+            p["@"] = "<" + config.get("application", 
+                "preferences-url").replace("/preferences", "") + ">"
+        if("AUTOFILL" in p["com:destination"]):
+            p["com:destination"] = \
+                "<" + config.get("application", "financial-account") + ">"
+
+    # Set the necessary asset/license variables
+    lc["ps:asset"] = asset["@"]
+    lc["ps:assetHash"] = hashlib.sha1(jsonld.normalize(asset)).hexdigest()
+    lc["ps:license"] = \
+        "<" + config.get("application", "default-license") + ">"
+    lc["ps:licenseHash"] = \
+        config.get("application", "default-license-hash")
+    lc["ps:validFrom"] = \
+        time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+    lc["ps:validUntil"] = \
+        time.strftime("%Y-%m-%dT%H:%M:%SZ", 
+            time.gmtime(time.time() + 60*60*24))
 
     # Digitally sign the listing
+    sl = signature.sign(config, lc)
 
     # Upload the listing
-    print storage_url
     req = urllib2.Request(storage_url,
         headers = { "Content-Type": "application/json" },
-        data = json.dumps(signed_listing, sort_keys = True, indent = 3))
+        data = json.dumps(sl, sort_keys = True, indent = 3))
     urllib2.urlopen(req)
 
